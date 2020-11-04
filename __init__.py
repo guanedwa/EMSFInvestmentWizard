f25787d348a1d608d5bdac84d12d5f21c0b721a0
guanedwa <46826279+guanedwa@users.noreply.github.com>
2020-11-04 13:32:53 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+
+import logging
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello world'
+
+    from InvestmentWizard import MainApp
+    app.register_blueprint(MainApp.bp)
+
+    return app
